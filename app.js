88356c0d87bf47f2eb288984636417cfa4c47364
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-09-06 15:31:43 +0200

    feat: use an app level Express middleware called urlencoded

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 8080;
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
 
 app.use("/", indexRouter);
