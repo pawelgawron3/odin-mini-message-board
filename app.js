3c736ef64636d7e89620a24f556c46c8a0fb7c14
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-09-06 10:56:44 +0200

    feat: add initial Express app setup

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Hello from server!");
+});
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+
+  console.log(`Server is listening for requests on port ${PORT}`);
+});
