f83b6c0876ae5e70cc4fbd9c9b6a93f9d7d20004
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-09-07 11:37:51 +0200

    feat: add date formatter helper function

---
+++ helpers/formatDate.js
@@ -0,0 +1,12 @@
+function formatDate(date) {
+  return new Intl.DateTimeFormat("pl-PL", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  }).format(date);
+}
+
+export default formatDate;
