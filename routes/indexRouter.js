b85ace650b06a049adadd97c6a939a7e97fe7893
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-09-07 10:48:16 +0200

    feat: add id prop to message, add new route

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,13 +2,16 @@ import Router from "express";
 
 const indexRouter = Router();
 
+let lastUsedID = 1;
 const messages = [
   {
+    id: 0,
     user: "Amando",
     text: "Hi there!",
     added: new Date(),
   },
   {
+    id: 1,
     user: "Charles",
     text: "Hello World!",
     added: new Date(),
@@ -19,8 +22,17 @@ indexRouter.get("/", (req, res) => {
   res.render("index", { title: "Mini Messageboard", messages });
 });
 
+indexRouter.get("/messageDetails/:id", (req, res) => {
+  const messageId = Number(req.params.id);
+
+  const message = messages.find((message) => message.id === messageId);
+
+  res.render("messageDetails", { message });
+});
+
 indexRouter.post("/", (req, res) => {
   const newMessage = {
+    id: ++lastUsedID,
     user: req.body.messageUser,
     text: req.body.messageText,
     added: new Date(),
